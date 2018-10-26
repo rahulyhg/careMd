da107d37dacd55e194edf360efdf851979466669
Rayton Kiwelu <kiwelurayton@gmail.com>
2018-10-26 17:36:54 +0300

    Fix Closing windows

--- modules/stock_tz/gui/gui_setup.php
+++ modules/stock_tz/gui/gui_setup.php
@@ -89,7 +89,7 @@ A:visited:hover {color: #cc0033;}
 			</TABLE>
 
 			<p>
-			<a href="../../main/startframe.php?ntid=false&lang=$lang"><img src="../../gui/img/control/default/en/en_close2.gif" border=0 width="103" height="24" alt="" style="filter:alpha(opacity=70)" onMouseover="hilite(this,1)" onMouseOut="hilite(this,0)"></a>
+			<a href="javascript:history.back()"><img src="../../gui/img/control/default/en/en_close2.gif" border=0 width="103" height="24" alt="" style="filter:alpha(opacity=70)" onMouseover="hilite(this,1)" onMouseOut="hilite(this,0)"></a>
 
 			<p>
 			</blockquote>									
