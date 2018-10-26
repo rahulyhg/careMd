b4d72d40539e38f9a590b908f0b299c300536e1e
israel pascal <israel.pascal10@gmail.com>
2018-10-26 12:35:50 +0300

    merge israel changes

--- generated-conf/config.php
+++ generated-conf/config.php
@@ -1,12 +1,14 @@
 <?php
+require_once("./roots.php");
+require_once($root_path."include/inc_init_main.php");
 $serviceContainer = \Propel\Runtime\Propel::getServiceContainer();
 $serviceContainer->checkVersion('2.0.0-dev');
 $serviceContainer->setAdapterClass('default', 'mysql');
 $manager = new \Propel\Runtime\Connection\ConnectionManagerSingle();
 $manager->setConfiguration(array (
-  'dsn' => 'mysql:host=localhost;port=3306;dbname=caredb_aicc',
-  'user' => 'root',
-  'password' => '20Kukzan09',
+  'dsn' => "mysql:host=localhost;port=3306;dbname=$dbname",
+  'user' => "$dbusername",
+  'password' => "$dbpassword",
   'settings' =>
   array (
     'charset' => 'utf8',
